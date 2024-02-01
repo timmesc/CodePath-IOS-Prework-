d6cec669eeb43e8e62046826397d4a795b9d4130
timmesc <118234989+timmesc@users.noreply.github.com>
2024-02-01 14:31:05 -0500

    Add files via upload

---
+++ CodePath/CodePath/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  CodePath
+//
+//  Created by Charles Timmes on 2/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func changeBackgroundColor(_ sender: UIButton, forEvent event: UIEvent) {
+        let randomColor = changeColor()
+           view.backgroundColor = randomColor
+    }
+    func changeColor() -> UIColor{
+
+           let red = CGFloat.random(in: 0...1)
+           let green = CGFloat.random(in: 0...1)
+           let blue = CGFloat.random(in: 0...1)
+
+           return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+       }
+
+}
+
